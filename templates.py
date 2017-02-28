d5c8c30c547eb0781e2951a1f7594738ec1b264b
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-02-28 18:14:36 +0100

    Template now extends base.html

--- templates.py
+++ templates.py
@@ -17,25 +17,10 @@ import jinja2
 import webapp2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
-
-
-# hidden_html = """
-# <input type="hidden" name="food" value="%s" />
-# """
-#
-# item_html = """
-# <li>%s</li>
-# """
-#
-# shopping_list_html = """
-# <br />
-# <br />
-# <h2>Shopping list</h2>
-# <ul>
-# %s
-# </ul>
-# """
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(template_dir),
+    autoescape = True
+    )
 
 
 class Handler(webapp2.RequestHandler):
@@ -53,26 +38,8 @@ class MainPage(Handler):
 
 
     def get(self):
-        self.render("shopping_list.html")
-
-        # output = form_html
-        # output_hidden = ""
-        #
-        # items = self.request.get_all("food")
-        # output_items = ""
-        # if items:
-        #     for item in items:
-        #         item = str(item)
-        #         output_hidden += hidden_html % item
-        #         output_items += item_html % item
-        #
-        #     output_shopping = shopping_list_html % output_items
-        #     output += output_shopping
-        #
-        # output = output % output_hidden
-        # self.write(output)
-
-
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items = items)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
