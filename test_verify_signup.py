b9927ea417f9afc7cb27b49b6f868c74661b35c8
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-03-04 13:40:03 +0100

    unittest for UserEntity

--- test_verify_signup.py
+++ test_verify_signup.py
@@ -3,17 +3,97 @@
 
 """Just a test for verifying signup_routine."""
 import sys
+import unittest
+# Make sure these folders point to the correct place
 sys.path.insert(1, '../google-cloud-sdk/platform/google_appengine')
 sys.path.insert(1, '../google-cloud-sdk/platform/google_appengine.ext')
-sys.path.insert(1, '../google-cloud-sdk/platform/google_appengine/lib/yaml/lib')
+sys.path.insert(
+    1, '../google-cloud-sdk/platform/google_appengine/lib/yaml/lib')
+from google.appengine.api import memcache  # noqa
+from google.appengine.ext import ndb  # noqa
+from google.appengine.ext import testbed  # noqa
+from verify_signup import (valid_email,  # noqa
+                           valid_password,
+                           valid_username,
+                           verify_passwords_matches,
+                           username_not_in_use
+                          )
+from Entities import UserEntity  # noqa
 
-import unittest
-from google.appengine.api import memcache
-from google.appengine.ext import ndb
-from google.appengine.ext import testbed
 
-from verify_signup import (valid_email, valid_password,
-                           valid_username, verify_passwords_matches)
+class TestModel(ndb.Model):
+    """A model class used for testing."""
+    number = ndb.IntegerProperty(default=42)
+    text = ndb.StringProperty()
+
+
+class TestEntityGroupRoot(ndb.Model):
+    """Entity group root"""
+    pass
+
+
+def GetEntityViaMemcache(entity_key):
+    """Get entity from memcache if available, from datastore if not."""
+    entity = memcache.get(entity_key)
+    if entity is not None:
+        return entity
+    key = ndb.Key(urlsafe=entity_key)
+    entity = key.get()
+    if entity is not None:
+        memcache.set(entity_key, entity)
+    return entity
+
+
+class DatastoreTestCase(unittest.TestCase):
+    """Class for doing Entity-tests. (google-code)."""
+
+    def setUp(self):
+        # First, create an instance of the Testbed class.
+        self.testbed = testbed.Testbed()
+        # Then activate the testbed, which prepares the service stubs for use.
+        self.testbed.activate()
+        # Next, declare which service stubs you want to use.
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+        # Clear ndb's in-context cache between tests.
+        # This prevents data from leaking between tests.
+        # Alternatively, you could disable caching by
+        # using ndb.get_context().set_cache_policy(False)
+        ndb.get_context().clear_cache()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+
+class UserEntityTest(DatastoreTestCase):
+    """Test for User Account Entity."""
+
+
+    def test_UserEntity_register(self):
+        """UserEntity.register should create the useraccount, with correct data."""
+        UserEntity.register(username='Jamie', password='password123').put()
+        user = UserEntity.gql("WHERE username = 'Jamie'")[0]
+        user2 = UserEntity.by_name('Jamie')
+        user3 = UserEntity.by_name('Kelly')
+        self.assertEqual(user.username, 'Jamie',
+                         'gql-query should return a user.')
+        self.assertEqual(user2.username, 'Jamie',
+                         'UserEntity.by_name should return a user for existin user..')
+        self.assertEqual(user3, None,
+                         'UserEntity.by_name should return None if the user does not exist')
+        self.assertNotEqual(user.password, 'password123',
+                            'userpassword should be hashed, not stored as plain text')
+        self.assertEqual(len(user.password), 60,
+                         'userpassword should be hashed to 60 characters')
+
+    def test_username_not_in_use(self):
+        """Tests for checking if a username is already taken"""
+        UserEntity.register(username='Jamie', password='password123').put()
+        self.assertTrue(username_not_in_use('Jimmy'),
+                        'username_not_in_use should return true if username not in use.')
+        self.assertFalse(username_not_in_use('Jamie'),
+                         'username_not_in_use should return false if username in use.')
+
 
 
 class UsernameTest(unittest.TestCase):
