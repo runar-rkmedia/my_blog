a107d99dbd4f15fbceb20791d37916738eebf90d
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-03-11 20:16:54 +0100

    made global variables local

--- verify_signup.py
+++ verify_signup.py
@@ -3,19 +3,18 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 import re
-USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-PASS_RE = re.compile(r"^.{3,20}$")
-EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
 
 
 def valid_username(username):
     """Check if a username is valid."""
-    return bool(USER_RE.match(username))
+    user_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+    return bool(user_re.match(username))
 
 
 def valid_password(password):
     """Check that the password is valid."""
-    return bool(PASS_RE.match(password))
+    pass_re = re.compile(r"^.{3,20}$")
+    return bool(pass_re.match(password))
 
 
 def verify_passwords_matches(password1, password2):
@@ -25,4 +24,5 @@ def verify_passwords_matches(password1, password2):
 
 def valid_email(email):
     """Check that the email-adress is valid."""
-    return bool(EMAIL_RE.match(email))
+    email_re = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+    return bool(email_re.match(email))
